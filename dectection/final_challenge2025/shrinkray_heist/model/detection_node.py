b4c92bc0985331c3263ee38920173b5da5d63fc8
Jialin Chen <jialinc7@mit.edu>
2025-04-30 17:09:08 -0400

    first commit

---
+++ dectection/final_challenge2025/shrinkray_heist/model/detection_node.py
@@ -0,0 +1,40 @@
+import rclpy
+from rclpy.node import Node
+
+from cv_bridge import CvBridge
+
+from sensor_msgs.msg import Image
+from .detector import Detector
+
+class DetectorNode(Node):
+    def __init__(self):
+        super().__init__("detector")
+        self.detector = Detector()
+        self.publisher = self.self.create_publisher(Image, "/prediced/image")
+        self.subscriber = self.create_subscription(Image, "/zed/zed_node/rgb/image_rect_color", self.callback, 1)
+        self.bridge = CvBridge()
+
+        self.get_logger().info("Detector Initialized")
+
+    def callback(self, img_msg):
+        # Process image with CV Bridge
+        image = self.bridge.imgmsg_to_cv2(img_msg, "bgr8")
+
+        self.detector.set_threshold(0.5)
+
+        results = self.detector.predict(image)
+
+        predictions = results["predictions"]
+        original_image = results["original_image"]
+
+        out = self.detector.draw_box(original_image, predictions, draw_all=True)
+        self.publisher.publush(out)
+
+def main(args=None):
+    rclpy.init(args=args)
+    detector = DetectorNode()
+    rclpy.spin(detector)
+    rclpy.shutdown()
+
+if __name__=="__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'dectection'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='racecar',
+    maintainer_email='jialinc7@mit.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'detection = dectection.final_challenge.shrinkray_heist.model.detection_node:main'
+        ],
+    },
+)
