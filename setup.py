de6e3984e678aa4cee3df8e70a7c630f27628027
Jialin Chen <jialinc7@mit.edu>
2025-05-01 22:46:59 -0400

    launch

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/dectection/launch/', glob.glob(os.path.join('launch', 'part_a', '*launch.*'))),
+        ('share/dectection/launch/', glob.glob(os.path.join('launch', '*launch.*'))),
         # (os.path.join('share', package_name, 'config', 'real'), glob.glob('config/real/*.yaml')),
     ],
     install_requires=['setuptools'],
