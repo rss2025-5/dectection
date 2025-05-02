771b8ea58832b96a6e6f154881ff70cacf7ad11c
Jialin Chen <jialinc7@mit.edu>
2025-05-01 20:39:28 -0400

    test

---
+++ dectection/drive_mux_node.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Bool
+from ackermann_msgs.msg import AckermannDriveStamped
+
+class DriveMux(Node):
+    def __init__(self):
+        super().__init__('drive_mux')
+
+        self.controller_mode = "FOLLOWING"  # FOLLOWING or AVOIDING
+
+        self.drive_pub = self.create_publisher(AckermannDriveStamped, "/vesc/low_level/input/navigation", 10)
+
+        self.follower_sub = self.create_subscription(AckermannDriveStamped, "/drive_follower", self.follower_cb, 10)
+        self.wall_sub = self.create_subscription(AckermannDriveStamped, "/drive_wall", self.wall_cb, 10)
+
+        self.obstacle_sub = self.create_subscription(Bool, "/obstacle_detected", self.obstacle_cb, 10)
+
+    def obstacle_cb(self, msg):
+        if msg.data and self.controller_mode != "AVOIDING":
+            self.get_logger().info("Obstacle detected → switching to WALL FOLLOWER")
+            self.controller_mode = "AVOIDING"
+        elif not msg.data and self.controller_mode != "FOLLOWING":
+            self.get_logger().info("Obstacle cleared → switching to TRAJECTORY FOLLOWER")
+            self.controller_mode = "FOLLOWING"
+
+    def follower_cb(self, msg):
+        if self.controller_mode == "FOLLOWING":
+            self.drive_pub.publish(msg)
+
+    def wall_cb(self, msg):
+        if self.controller_mode == "AVOIDING":
+            self.drive_pub.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    mux = DriveMux()
+    rclpy.spin(mux)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -21,6 +21,9 @@ setup(
     entry_points={
         'console_scripts': [
             'detection = dectection.final_challenge2025.shrinkray_heist.model.detection_node:main'
+            'drive = dectection.drive_mux_node:main'
+            'state = dectection.state_machine_nodee:main'
+
         ],
     },
 )
