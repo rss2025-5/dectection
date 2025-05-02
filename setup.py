f92d833986ea2cdb42b12e8ea20c0398945c3aa8
Jialin Chen <jialinc7@mit.edu>
2025-05-01 21:06:34 -0400

    typo

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'console_scripts': [
             'detection = dectection.final_challenge2025.shrinkray_heist.model.detection_node:main'
             'drive = dectection.drive_mux_node:main'
-            'state = dectection.state_machine_nodee:main'
+            'state = dectection.state_machine_node:main'
 
         ],
     },
