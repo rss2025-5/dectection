b4c32d65d2f038b28b26cbf7a23561e948b7424f
Jialin Chen <jialinc7@mit.edu>
2025-05-10 13:38:27 -0400

    update traffic img

--- dectection/final_challenge2025/shrinkray_heist/model/detection_node.py
+++ dectection/final_challenge2025/shrinkray_heist/model/detection_node.py
@@ -66,6 +66,12 @@ class DetectorNode(Node):
                 #t_msg = img_msg
                 self.traffic.publish(t_msg)
                 break
+            else:
+                black_img = np.zeros((360, 640, 3), dtype=np.uint8)
+                black_img = self.bridge.cv2_to_imgmsg(black_img, encoding="bgr8")
+
+                self.traffic.publish(black_img)
+                self.get_logger().info('Published black image.')
 
         #self.publisher.publish(out_msg)
 
